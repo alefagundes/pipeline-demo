3c401cd06180bffcb4ef9644355f91c25f5d49da
alefagundes <alefagundes2000.af@gmail.com>
2021-11-07 12:19:44 -0300

    pipe + function .filter() + .Stream()

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -13,15 +14,14 @@ public class Program {
 		System.out.println(Arrays.toString(st1.toArray()));
 		
 		
-		Stream<String> st2 = Stream.of("Maria", "alex", "bob");
-		System.out.println(Arrays.toString(st2.toArray()));
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		System.out.println("sum = " + sum);
 		
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		List<Integer> list2 = list.stream().filter(x -> x % 2 == 0).map(x -> x*10).collect(Collectors.toList());
 		
-		Stream<Long> fibonacci = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]); 
-		System.out.println(Arrays.toString(fibonacci.limit(10).toArray()));
+		System.out.println(Arrays.toString(list2.toArray()));
 
 	}
+	
 
 }
